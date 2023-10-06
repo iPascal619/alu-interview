68100b1dc8548cdebf233bfb9f583de6df1cab81
iPascal619 <c.onuoha@alustudent.com>
2023-10-06 21:42:50 +0000

    TAsk 0

---
+++ rain/0-rain.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Description: Given a list of non-negative integers representing walls of
+width 1, calculate how much water will be retained after it rains.
+"""
+
+
+def rain(walls):
+    """
+    Args:
+        walls - is a list of non-negative integers.
+    Returns:
+        If the list is empty return 0.
+    """
+    n = len(walls)
+    total_water = 0
+
+    for i in range(1, n - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+
+        for j in range(i + 1, n):
+            right = max(right, walls[j])
+
+        total_water = total_water + (min(left, right) - walls[i])
+    return total_water
